45fc9d076fb11bdad98918561a5706b48adc1c8a
Markus Amano <markus.a.amano@gmail.com>
2024-11-12 22:14:10 +0900

    update

--- summarizeweather.jl
+++ summarizeweather.jl
@@ -6,47 +6,39 @@ const ST{G} = Series{Number,Tuple{Mean{G,EqualWeight},Variance{G,G,EqualWeight}}
     return Series(Mean(T), Variance(T))
 end
 
-function csvstream(io, stringtype=String, numbertype=Float64, delim=';', endline='\n', chunk_size=1000000)
-    # readuntil(stream::IO, delim; keep::Bool = false)
-    Channel() do ch
-        while !eof(io)
-            city = stringtype[]
-            temperature = numbertype[]
-            sizehint!(city, chunk_size)
-            sizehint!(temperature, chunk_size)
-            for _ in 1:chunk_size
-                eof(io) && break
-                push!(city, readuntil(io, delim))
-                push!(temperature, parse(numbertype, readuntil(io, endline)))
-            end
-            put!(ch, (city=city, temperature=temperature))
-        end
-    end
-end
-
 function (@main)(_)
 
-    # rows = CSV.Chunks("mystats.csv"; header=[:city, :temperature], delim=';', types=[String, Float64])
+    #     rows = CSV.Rows("mystats.csv"; reusebuffer = true, header=[:city, :temperature], delim=';', types=[Symbol, Float64])
+    #     o = GroupBy(Symbol, newstats(Float64))
+    #     fit!(o, (city => temperature for (; city, temperature) in rows))
+
+    chunks = CSV.Chunks("mystats.csv", header=[:city, :temperature], delim=';', types=String, ntasks=1000)
     # chunks = CSV.Chunks("mystats.csv", header=[:city, :temperature], delim=';', types=[String, Float64], ntasks=1000)
 
     ch = Channel(Threads.nthreads()) do c
-        open("mystats.csv") do io
-            foreach(chunk -> put!(c, chunk), csvstream(io))
-            while true
-                put!(c, nothing)
-            end
+        for chunk in chunks
+            put!(c, chunk)
+        end
+
+        while true
+            put!(c, nothing)
         end
     end
 
     tsks = map(1:Threads.nthreads()) do _
         Threads.@spawn begin
             records = Dict{String,ST{Float64}}()
+            # records = GroupBy(Symbol, newstats(Float64))
             while true
                 chunk = take!(ch)
                 isnothing(chunk) && break
 
-                df = DataFrame(chunk)
-                @info "Chuck Data" thread=Threads.threadid() size=nrow(df)
+                df = DataFrame(
+                    (city=city, temperature=parse(Float64, temperature))
+                    for (; city, temperature) in chunk
+                )
+
+                @info "Chunk Data" thread = Threads.threadid() size = nrow(df)
 
                 for gdf in groupby(df, :city)
                     city = first(gdf.city)
@@ -64,7 +56,6 @@ function (@main)(_)
     for tsk in tsks
         mergewith!(merge!, total_records, fetch(tsk))
     end
-
 
     for r in total_records
         println(r)
